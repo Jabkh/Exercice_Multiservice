ec56783bd8818d031e302a0eea55d4c7feeca6c5
JaberKh <j.khaloua@hotmail.com>
2024-08-14 16:31:21 +0200

    comit fait 1

---
+++ infrastructure_restclient/src/test/java/org/example/infrastructure_restclient/InfrastructureRestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.infrastructure_restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrastructureRestclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
